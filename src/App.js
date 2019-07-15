bb08df91646ce112c9e12b39c18977bc0d7275fa
Rachel Meyerchick <rachelmeyerchick@Rachels-MacBook-Pro.local>
2019-07-15 15:58:08 -0500

    add client dashboard styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import JobListings from './components/JobListings';
+import ClientDashboard from './components/clientDashboard/clientDashboard';
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     <JobListings/>
+      <JobListings/>
+      <ClientDashboard />
     </div>
   );
 }
