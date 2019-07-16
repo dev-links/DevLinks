053e17e718b2fbc8b03ddb1bbbad14b8910059c1
Rachel Meyerchick <rachelmeyerchick@Rachels-MacBook-Pro.local>
2019-07-16 16:21:22 -0500

    Add styling

--- src/App.js
+++ src/App.js
@@ -15,8 +15,8 @@ function App() {
       {/* <ClientDashboard /> */}
       {/* <Landing /> */}
       {/* <Login /> */}
-      {/* <ClientRegister /> */}
-      <AdminRegister />
+      <ClientRegister />
+      {/* <AdminRegister /> */}
     </div>
   );
 }
