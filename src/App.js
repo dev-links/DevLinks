3ae40fb413d130fd8e221f7e82dc4f84796feaa8
Chris <cabiediger@gmail.com>
2019-07-16 16:47:38 -0500

    add style add functionality to Chat

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import JobListings from './components/JobListings';
 import ClientDashboard from './components/clientDashboard/clientDashboard';
 import Landing from './components/Landing/Landing';
 import Login from './components/Login/Login';
+import Chatroom from './components/ChatRoom'
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <JobListings/>
+      {/* <JobListings/> */}
       {/* <ClientDashboard /> */}
       {/* <Landing /> */}
       {/* <Login /> */}
+      <Chatroom />
     </div>
   );
 }
