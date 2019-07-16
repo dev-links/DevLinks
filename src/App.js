3c94c4a7403b633eb8b95775a13e8314ff077808
Rachel Meyerchick <rachelmeyerchick@Rachels-MacBook-Pro.local>
2019-07-16 09:45:23 -0500

    Add styling to landing page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import JobListings from './components/JobListings';
 import ClientDashboard from './components/clientDashboard/clientDashboard';
+import Landing from './components/Landing/Landing';
 
 import './App.css';
 
@@ -8,7 +9,8 @@ function App() {
   return (
     <div className="App">
       <JobListings/>
-      <ClientDashboard />
+      {/* <ClientDashboard /> */}
+      {/* <Landing /> */}
     </div>
   );
 }
