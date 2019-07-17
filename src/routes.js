a2885fff1537a15a87679c1c162bb96f57f310f8
Chris <cabiediger@gmail.com>
2019-07-17 11:58:21 -0500

    add updated message db, fixed input value bug

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@ import { Switch, Route } from "react-router-dom";
 import App from './App';
 import Step1 from './components/JobWizard/Step1';
 import Step2 from './components/JobWizard/Step2';
-
+import ChatRoom from './components/ChatRoom'
 import Landing from './components/Landing/Landing';
 import Login from './components/Login/Login';
 import ClientRegister from './components/clientRegister/clientRegister';
@@ -19,5 +19,6 @@ export default (
         <Route path='/login' component={Login} />
         <Route path='/client-register' component={ClientRegister} />
         <Route path='/admin-register' component={AdminRegister} />
+        <Route path='/chat' component={ChatRoom} />
     </Switch>
 )
