2bc1ef7b4854ea65175353f82f5d6e6617c2f573
Collins Evulukwu <siranaba@Collinss-MacBook-Air.local>
2019-07-17 09:48:33 -0500

    added nav bar component

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import { Switch, Route } from "react-router-dom";
 import App from './App';
 import Step1 from './components/JobWizard/Step1';
 import Step2 from './components/JobWizard/Step2';
+import NavBar from './components/NavBar/NavBar';
 
 export default (
     <Switch>
