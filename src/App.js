4e3dfb24c1b957a5dfa08f28a07618206b77767d
Bryan <Jbrewton88@gmail.com>
2019-07-19 12:21:06 -0500

    Set up toekn for routing

--- src/App.js
+++ src/App.js
@@ -16,16 +16,19 @@ import firebaseStore from './redux/firebaseStore';
 import axios from 'axios'
 import jwtDecode from 'jwt-decode';
 
+let authenticated
 const token = localStorage.FBIdToken;
 if(token){
   const decodeToken = jwtDecode(token);
   if(decodeToken.exp * 1000 < Date.now()){
-    firebaseStore.dispatch(logOutUser())
+//     firebaseStore.dispatch(logOutUser())
     window.location.href = '/#/login'
+    authenticated = false
   } else{
-    firebaseStore.dispatch({ type: "SET_AUTHENTICATED"})
-    axios.defaults.headers.common['Authorization'] = token
-    firebaseStore.dispatch(getUserData())
+//     firebaseStore.dispatch({ type: "SET_AUTHENTICATED"})
+//     axios.defaults.headers.common['Authorization'] = token
+//     firebaseStore.dispatch(getUserData())
+        authenticated = true
   }
 }
 
@@ -33,7 +36,7 @@ function App() {
   return (
     <Router>
     <div className="App">
-      <JobListings/>
+      {/* <JobListings/> */}
       {/* <ClientDashboard /> */}
       {/* <Landing /> */}
       {/* <Login /> */}
@@ -47,11 +50,11 @@ function App() {
       {/* <Landing /> */}
       {/* <Login /> */}
 
-      {/* <ClientRegister /> */}
+      <ClientRegister />
 
 
 
-      {/* {routes} */}
+      {routes}
     </div>
     </Router>
   );
