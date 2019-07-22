59c54dc55ed6a2a4fb18a513e4903d2c970b7c6e
Chris <cabiediger@gmail.com>
2019-07-22 09:13:38 -0500

    fix jest bug

--- src/routes.js
+++ src/routes.js
@@ -7,7 +7,7 @@ import ChatRoom from './components/Chat/ChatRoom'
 import Landing from './components/Landing/Landing';
 import Login from './components/Login/Login';
 import ClientRegister from './components/clientRegister/clientRegister';
-import AdminRegister from './components/adminRegister/AdminRegister';
+import AdminRegister from './components/adminRegister/adminRegister';
 import ClientDashboard from './components/clientDashboard/clientDashboard';
 import AdminDashboard from './components/adminDashboard/adminDashboard';
 import ChatDashboard from './components/Chat/ChatDashboard'
