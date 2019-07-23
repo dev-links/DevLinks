33a6526ede0a0ff9dec04ef6c6625ce977764738
Rachel Meyerchick <rachelmeyerchick@Rachels-MacBook-Pro.local>
2019-07-23 15:26:45 -0500

    Register

--- cypress/integration/rachel_list_spec.js
+++ cypress/integration/rachel_list_spec.js
@@ -1,1 +1,69 @@
-g
+// UNIT TESTS
+describe('Input password Changes', function() {
+    context('See letters appear', function() {
+        it('change email input', function() {
+            cy.visit('http://localhost:9009/#/')
+            cy.get('#email').type('Berries')
+            cy.get('#email').should('have.value', 'Berries')
+        })
+    })
+})
+
+describe('Input email Changes', function() {
+    context('See letters appear', function() {
+        it('change password input', function() {
+            cy.visit('http://localhost:9009/#/')
+            cy.get('#password').type('Avacados')
+            cy.get('#password').should('have.value', 'Avacados')
+        })
+    })
+})
+
+describe('Conditional Rendering to Register', function() {
+    context('Register will appear', function() {
+        it('switches to register', function() {
+            cy.visit('http://localhost:9009/#/')
+            cy.get('#register-button').click()
+            cy.get('input[name=username]')
+            cy.get('input[name=email]')
+            cy.get('input[name=password]')
+        })
+        it('switches to login', function(){
+            cy.get('#login-button').click()
+            cy.get('input[name=email]')
+            cy.get('input[name=password]')
+        })
+    })
+})
+
+describe('Input Check', function() {
+    it('Checks for input on page', function() {
+        cy.visit('http://localhost:9009/#/')
+        cy.get('Input')
+    })
+})
+
+// END TO END TESTS
+describe('Logging In', function () {
+// we can use these values to log in
+    const email = 'bryantest@gmail.com'
+    const password = 'test1234'
+
+    context('HTML form submission', function () {
+        beforeEach(function () {
+            cy.visit('http://localhost:9009/#/')
+        })
+
+        it('displays errors on login', function () {
+          // incorrect email on purpose
+            cy.get('#email').type('jane.lae')
+            cy.get('#password').type('password123{enter}')
+        })
+
+        it('redirects to /client-dashboard on success', function () {
+            cy.get('#email').type(email)
+            cy.get('#password').type(password)
+            cy.get('#login-button').click()
+        })
+    })
+})
