aa2926f395594ce627733058e880e67e9a6a180f
Rachel Meyerchick <rachelmeyerchick@Rachels-MacBook-Pro.local>
2019-07-23 13:48:21 -0500

    login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,140 +1,311 @@
 import React, {Component} from 'react';
+
 import {Link, Redirect} from 'react-router-dom';
-import axios from 'axios'
+
+import axios from 'axios';
+
 import {connect} from 'react-redux';
+
 import PropTypes from 'prop-types';
+
 import './Login.css';
-import {Button, Input} from 'reactstrap'
+
+// import {Input} from 'reactstrap';
+
+import Button from '@material-ui/core/Button';
+
+
 
 class Login extends Component {
+
     constructor(props) {
+
         super(props);
+
         this.state = {
+
             email: '',
+
             password: '',
+
             errors: {},
+
             loading: false,
+
             redirect: false
+
         }
+
     } 
 
+
+
     handleClick = (e) => {
+
         e.preventDefault();
+
         this.setState({
+
             loading : true
+
         })
+
         const userData = {
+
             email : this.state.email,
+
             password : this.state.password
+
         }
+
         axios.post('/login', userData)
+
         .then(res => {
+
             console.log(res.data)
+
             localStorage.setItem('FBIdToken', `Bearer ${res.data.token}`)
+
             this.setState({
+
                 loading: false
+
             })
+
             this.props.history.push('/client-dashboard')
+
         })
+
         .catch(err => {
+
             this.setState({
+
                 errors: err.data,
+
                 loading: false
+
             })
+
         })
+
     }
 
+
+
     handleChange = (e) => {
+
         console.log(e.target.value)
+
         this.setState({
+
             [e.target.name]: e.target.value
+
         });
+
     }
+
+
 
     render() {
 
-        //Checking to see if client or admin
-        // if(this.state.redirect === true && this.state.user.admin === true) {
-        //     console.log('Logged in as Employer')
-        //     return <Redirect to='admin-dashboard' />
-        // }
-        // if(this.state.redirect === true && this.state.user.admin === false) {
-        //     console.log('Logged in as Client')
-        //     return <Redirect to='client-dashboard' />
-        // }
+
 
         return (
 
+
+
             <div className='login-container'>
+
                 <div className='login'>
-                    <h1><span style={{ color: '#F8E60B'}}>DEV</span> LINKS</h1>
-                    <h3>Username</h3>
+
+                    <span id='login-dev'>DEV</span> <span id='login-links'>LINKS</span>
+
+                    {/* <h3>Username</h3>
+
                     <Input type='text' id='email'
+
                     name='email' type='email' value={this.state.email} onChange={this.handleChange}
-                    />
-                    <h3>Password</h3>
+
+                    /> */}
+
+                    {/* <h3>Password</h3>
+
                     <Input type='password' id='password'
+
                     name='password' type='password' value={this.state.password} onChange={this.handleChange}
-                    />
+
+                    /> */}
+
                     
-                    <Button id='loginBtn'
+
+                    <Button onClick={this.handleClick} id='login-button' variant="contained" color="primary">
+
+                    Login
+
+                    </Button>
+
+
+
+                    <Link to='/admin-register'><Button id='register-button' variant="contained" color="primary">
+
+                    Register
+
+                    </Button></Link>
+
+
+
+                    <form action="">
+
+                        <div id="email-input-field">
+
+                            <input type='text' id='email'
+
+                            name='email' type='email' value={this.state.email} onChange={this.handleChange} required />
+
+                            <label for="name">Email:</label>
+
+                        </div>
+
+                    </form>
+
+
+
+                    <form action=''>
+
+                        <div id='password-input-field'>
+
+                            <input type='password' id='password'
+
+                            name='password' type='password' value={this.state.password} onChange={this.handleChange} required />
+
+                            <label for='name'>Password:</label>
+
+                        </div>
+
+                    </form>
+
+
+
+                <div class='lds-ripple'><div></div><div></div></div>
+
+
+
+                    {/* <Button id='loginBtn'
+
                     onClick={this.handleClick}
-                    >Login</Button>
+
+                    >Login</Button> */}
+
                     
-                    <Link to='/admin-register'>
+
+                    {/* <Link to='/admin-register'>
+
                     <Button >Register</Button>
-                    </Link>
-                <div className="altSignIn">
+
+                    </Link> */}
+
+                <div className='altSignIn'>
+
                 {/* GITHUB */}
-                <img className='github' src='https://cdn0.iconfinder.com/data/icons/octicons/1024/mark-github-256.png' />
+
+                {/* <img className='github' src='https://cdn0.iconfinder.com/data/icons/octicons/1024/mark-github-256.png' /> */}
+
+
 
                 {/* GOOGLE */}
-                <img className='google' src='https://cdn1.iconfinder.com/data/icons/social-media-icon-1/112/google-plus-256.png' />
+
+                {/* <img className='google' src='https://cdn1.iconfinder.com/data/icons/social-media-icon-1/112/google-plus-256.png' /> */}
+
                 </div>
+
                 </div>
+
                 
+
+
 
             </div>
 
 
+
+
+
             // <div className='login-container'>
+
+
 
             //     <div id= 'landing-image-container'><img id='landing-image' src='https://images.pexels.com/photos/1366919/pexels-photo-1366919.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500' /></div>
 
 
+
+
+
             //     <Link to='/'><div className='dev'>
+
             //         DEV
+
             //     </div></Link>
 
+
+
             //     <Link to='/'><div className='links'>
+
             //         LINKS
+
             //     </div></Link>
+
+
 
             //     <h1 className='login-username-placeholder'>USERNAME</h1>
 
+
+
             //     <input id='login-username' className='input'  name='email' type='email' value={this.state.email} onChange={this.handleChange}/>
+
+
 
             //     <h1 className='login-password-placeholder'>PASSWORD</h1>
 
+
+
             //     <input id='login-password' className='input' name='password' type='password' value={this.state.password} onChange={this.handleChange} />
 
+
+
             //     <button className='login-button' onClick={this.handleClick} >
+
             //         LOGIN
+
             //     </button>
 
+
+
             //     <div className='or'>
+
             //         OR
+
             //     </div>
 
+
+
             //         {/* GITHUB */}
+
             //         <img className='github' src='https://cdn0.iconfinder.com/data/icons/octicons/1024/mark-github-256.png' />
 
+
+
             //         {/* GOOGLE */}
+
             //         <img className='google' src='https://cdn1.iconfinder.com/data/icons/social-media-icon-1/112/google-plus-256.png' />
+
             // </div>
+
         )
+
     }
+
 }
+
 
 
 export default Login;
