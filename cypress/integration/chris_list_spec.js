e47baf5f236e34699eaa12b4d0197f0a5a4c40db
Chris <cabiediger@gmail.com>
2019-07-25 09:47:28 -0500

    fix chris cypress test

--- cypress/integration/chris_list_spec.js
+++ cypress/integration/chris_list_spec.js
@@ -1,22 +1,23 @@
 describe('login actually works', () => {
     it('Should load the page', () => {
-        cy.visit('http://localhost:9009/#/login')
+        //  http://localhost:9009/#/login
+        cy.visit('http://165.22.173.29:9009/#/login')
     })
 })
  
 describe('Add Item Input Field', () => {
     it('Should be able to type in the email input field', () => {
-        cy.get('#email')
+        cy.get('#email').click({force: true})
         .type('bryantest@gmail.com')
         .should('have.value', 'bryantest@gmail.com')
     })
     it('Should be able to type in the password input field', () => {
-        cy.get('#password')
+        cy.get('#password').click({force: true})
         .type('test1234')
         .should('have.value', 'test1234')
     })
     it('Should log in', () => {
-        cy.get('#loginBtn')
+        cy.get('#login-button')
         .click()
 
         
@@ -25,13 +26,13 @@ describe('Add Item Input Field', () => {
 
 describe('Chat actually works', () => {
     it('Should load the page', () => {
-        cy.visit('http://localhost:9011/#/chat')
+        cy.visit('http://165.22.173.29:9009/#/chat')
     })
 })
 
 describe('Add Item Input Field', () => {
     it('Should be able to type in the input field', () => {
-        cy.get('#input')
+        cy.get('#input').click({force: true})
         .type('input typing works')
         .should('have.value', 'input typing works')
     })
